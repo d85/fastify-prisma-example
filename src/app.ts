40fab0d11018cc310ab59f3f0604c4d78cccbff9
Dan Carlier <dan.carlier@gmail.com>
2023-02-08 16:45:28 +1100

    Initialise Fastify, add healthcheck route

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('hello world')
+import Fastify from "fastify"
+
+const server = Fastify()
+
+server.get('/healthcheck', async function() {
+  return { status: "OK" }
+})
+
+async function main() {
+  try {
+    await server.listen(3000, '0.0.0.0')
+
+    console.log(`Server ready at http://localhost:3000`)
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+}
+
+main()
